c789b896383a5c11cefeedeaf104f7b6bbf95d9e
diarmuid. J <diarmuid451@gmail.com>
2019-12-10 17:21:28 +0900

    2019-12-10
    7장 Inheritance

--- src/z_exam/Exam06.java
+++ src/z_exam/Exam06.java
@@ -1,6 +1,58 @@
 package z_exam;
 
-public class Exam06 {
+public class Exam06 { 
+	public static int abs(int value){ //6-24 메서드		
+		return value >=0 ? value : -value;
+		
+	}
+	
+	public static int max(int[] arr){ //6-23 메서드
+		if(arr == null || arr.length == 0 ){
+			return -9999;
+		}
+		int max = arr[0];
+		for(int i = 1; i <arr.length; i++ ) {
+			if(max<arr[i]) max =arr[i];
+		}
+			return max;
+	}
+	
+	public static boolean isNumber(String str){	//6-22 메서드
+		if(str == null || str.equals("")){
+			return false;
+		} 
+		
+		for(int i = 0; i <str.length(); i++){
+			char ch = str.charAt(i);
+			if(ch < '0' || ch > '9'){
+				return false;} 
+			}	
+		return true;
+		
+	}
+	
+	public static int[] shuffle(int[] arr) { //6-20 메서드
+		if(arr == null || arr.length==0)
+		return arr;
+		for(int i=0; i< arr.length;i++) {
+		int j = (int)(Math.random()*arr.length);
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+		}
+		return arr;
+		}
+	
+	public static String change(String str) { //6-19
+		str += "456";
+		return str;
+		//>> String 타입이기 때문에 str 주소가 변경
+	}
+	
+	public static void change1(int xx) { 
+		xx += 456;
+		
+	}
 	public static void main(String[] args) {		
 		/*
 		[6-1] 다음과 같은 멤버변수를 갖는 SutdaCard클래스를 정의하시오.
@@ -124,55 +176,55 @@ public class Exam06 {
 		}	*/
 
 	/* [6-10] 다음 중 생성자에 대한 설명으로 옳지 않은 것은? (모두 고르시오)
-		a. 모든 생성자의 이름은 클래스의 이름과 동일해야한다.
-		b. 생성자는 객체를 생성하기 위한 것이다.
+		a. 모든 생성자의 이름은 클래스의 이름과 동일해야한다. 
+		b. 생성자는 객체를 생성하기 위한 것이다. x
 		c. 클래스에는 생성자가 반드시 하나 이상 있어야 한다.
 		d. 생성자가 없는 클래스는 컴파일러가 기본 생성자를 추가한다.
-		e. 생성자는 오버로딩 할 수 없다.
+		e. 생성자는 오버로딩 할 수 없다. x
 	
 		[6-11] 다음 중 this에 대한 설명으로 맞지 않은 것은? (모두 고르시오)
 		
 		a. 객체 자신을 가리키는 참조변수이다.
-		b. 클래스 내에서라면 어디서든 사용할 수 있다.
+		b. 클래스 내에서라면 어디서든 사용할 수 있다. x
 		c. 지역변수와 인스턴스변수를 구별할 때 사용한다.
 		d. 클래스 메서드 내에서는 사용할 수 없다.
 		
 		[6-12] 다음 중 오버로딩이 성립하기 위한 조건이 아닌 것은? (모두 고르시오)
 		a. 메서드의 이름이 같아야 한다.
-		b. 매개변수의 개수나 타입이 달라야 한다.
-		c. 리턴타입이 달라야 한다.
-		d. 매개변수의 이름이 달라야 한다.
+		b. 매개변수의 개수나 타입이 달라야 한다. 
+		c. 리턴타입이 달라야 한다. x
+		d. 매개변수의 이름이 달라야 한다. x
 		
 		[6-13] 다음 중 아래의 add메서드를 올바르게 오버로딩 한 것은? (모두 고르시오)
 		
 		long add(int a, int b) { return a+b;}
 		a. long add(int x, int y) { return x+y;}
-		b. long add(long a, long b) { return a+b;}
-		c. int add(byte a, byte b) { return a+b;}
-		d. int add(long a, int b) { return (int)(a+b);}
+		b. long add(long a, long b) { return a+b;} o
+		c. int add(byte a, byte b) { return a+b;} o
+		d. int add(long a, int b) { return (int)(a+b);} o
 		
 		[6-14] 다음 중 초기화에 대한 설명으로 옳지 않은 것은? (모두 고르시오)
 				
 		a.멤버변수는 자동 초기화되므로 초기화하지 않고도 값을 참조할 수 있다.
 		b.지역변수는 사용하기 전에 반드시 초기화해야 한다.
-		c.초기화 블럭보다 생성자가 먼저 수행된다.
-		d.명시적 초기화를 제일 우선적으로 고려해야 한다.
-		e.클래스변수보다 인스턴스변수가 먼저 초기화된다.
+		c.초기화 블럭보다 생성자가 먼저 수행된다. x
+		d.명시적 초기화를 제일 우선적으로 고려해야 한다. 
+		e.클래스변수보다 인스턴스변수가 먼저 초기화된다. x
 		
 		[6-15] 다음중 인스턴스변수의 초기화 순서가 올바른 것은?
 		
-		a. 기본값-명시적초기화-초기화블럭-생성자
+		a. 기본값-명시적초기화-초기화블럭-생성자 o
 		b. 기본값-명시적초기화-생성자-초기화블럭
 		c. 기본값-초기화블럭-명시적초기화-생성자
 		d. 기본값-초기화블럭-생성자-명시적초기화
 		
 		[6-16] 다음 중 지역변수에 대한 설명으로 옳지 않은 것은? (모두 고르시오)
 		
-		a. 자동 초기화되므로 별도의 초기화가 필요없다.
+		a. 자동 초기화되므로 별도의 초기화가 필요없다. x
 		b. 지역변수가 선언된 메서드가 종료되면 지역변수도 함께 소멸된다.
-		c. 매서드의 매개변수로 선언된 변수도 지역변수이다.
-		d. 클래스변수나 인스턴스변수보다 메모리 부담이 적다.
-		e. 힙(heap)영역에 생성되며 가비지 컬렉터에 의해 소멸된다.
+		c. 매서드의 매개변수로 선언된 변수도 지역변수이다. 
+		d. 클래스변수나 인스턴스변수보다 메모리 부담이 적다. 
+		e. 힙(heap)영역에 생성되며 가비지 컬렉터에 의해 소멸된다. x
 		
 		[6-17] 호출스택이 다음과 같은 상황일 때 옳지 않은 설명은? (모두 고르시오)
 		
@@ -181,52 +233,62 @@ public class Exam06 {
 		method2
 		main
 		a. 제일 먼저 호출스택에 저장된 것은 main메서드이다.
-		b. println메서드를 제외한 나머지 메서드들은 모두 종료된 상태이다.
-		c. method2메서드를 호출한 것은 main메서드이다.
-		d. println메서드가 종료되면 method1메서드가 수행을 재개한다.
-		e. main-method2-method1-println의 순서로 호출되었다.
-		f. 현재 실행중인 메서드는 println 뿐이다.*/
+		b. println메서드를 제외한 나머지 메서드들은 모두 종료된 상태이다. x
+		c. method2메서드를 호출한 것은 main메서드이다. 
+		d. println메서드가 종료되면 method1메서드가 수행을 재개한다. 
+		e. main-method2-method1-println의 순서로 호출되었다. 
+		f. 현재 실행중인 메서드는 println 뿐이다. 
+		*/ 
 		
 		/*[6-18] 다음의 코드를 컴파일하면 에러가 발생한다. 컴파일 에러가 발생하는 라인과 그 이유를 설명하시오.
 		
 		class MemberCall {
 		int iv = 10;
 		static int cv = 20;
+		
 		int iv2 = cv;
-		static int cv2 = iv; // 라인 A
+		static int cv2 = iv; // 라인 A ?
+		
 		static void staticMethod1() {
 		System.out.println(cv);
-		System.out.println(iv); // 라인 B
+		System.out.println(iv); // 라인 B ?
 		}
+		
 		void instanceMethod1() {
 		System.out.println(cv);
 		System.out.println(iv); // 라인 C
 		}
+		
 		static void staticMethod2() {
 		staticMethod1();
-		instanceMethod1(); // 라인 D
+		instanceMethod1(); // 라인 D ?
 		}
+		
 		void instanceMethod2() {
 		staticMethod1(); // 라인 E
 		instanceMethod1();
-		}
-		}
+		}		
+	}
 		*/
 
 //		[6-19] 다음 코드의 실행 결과를 예측하여 적으시오.
 //		
-//		class Exercise6_19 {
-//		public static void change(String str) {
-//		str += "456";
-//		}
 //		
 //		
-//		String str = "ABC123";
-//		System.out.println(str);
-//		change(str);
-//		System.out.println("After change:"+str);
-//		}
-//		
+		
+		String str = "ABC123";
+		System.out.println(str);
+		change(str);
+		System.out.println(change(str));
+		System.out.println("After change:"+str);
+		
+		int xx = 123;
+		System.out.println(xx);
+		change1(xx);
+		System.out.println("After change:"+ xx);
+
+		
+		//		[ABC123 / After change:ABC123]
 
 		/*[6-20] 다음과 같이 정의된 메서드를 작성하고 테스트하시오.
 		[주의] Math.random()을 사용하는 경우 실행결과와 다를 수 있음.
@@ -236,18 +298,15 @@ public class Exam06 {
 		반환타입 : int[]
 		매개변수 : int[] arr - 정수값이 담긴 배열*/
 		
-//		class Exercise6_20
-//		{
+//		
 //		/*
 //		(1) shuffle메서드를 작성하시오.
 //		*/
 //
-//		int[] original = {1,2,3,4,5,6,7,8,9};
-//		System.out.println(java.util.Arrays.toString(original));
-//		int[] result = shuffle(original);
-//		System.out.println(java.util.Arrays.toString(result));
-//
-//		}
+		int[] original = {1,2,3,4,5,6,7,8,9};
+		System.out.println(java.util.Arrays.toString(original));
+		int[] result = shuffle(original);
+		System.out.println(java.util.Arrays.toString(result));
 	
 //		[실행결과]
 //		[1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -265,11 +324,11 @@ public class Exam06 {
 //		final int MAX_CHANNEL = 100;
 //		final int MIN_CHANNEL = 1;
 //		void turnOnOff() {
-//		// (1) isPowerOn의 값이 true면 false로, false면 true로 바꾼다.
+//		isPowerON = !isPowerON
 //		}
 //		42 
 //		void volumeUp() {
-//		// (2) volume의 값이 MAX_VOLUME보다 작을 때만 값을 1증가시킨다.
+//		if(
 //		}
 //		void volumeDown() {
 //		// (3) volume의 값이 MIN_VOLUME보다 클 때만 값을 1감소시킨다.
@@ -313,17 +372,14 @@ public class Exam06 {
 		매개변수 : String str - 검사할 문자열
 		[Hint] String클래스의 charAt(int i)메서드를 사용하면 문자열의 i번째 위치한 문자를 얻을 수 있다.*/
 		
-//		class Exercise6_22 {
-//		/*
-//		(1) isNumber메서드를 작성하시오.
-//		*/
-//		
-//		String str = "123";
-//		System.out.println(str+"는 숫자입니까? "+isNumber(str));
-//		str = "1234o";
-//		System.out.println(str+"는 숫자입니까? "+isNumber(str));
-//		
-//		}
+
+
+		String str1 = "123";
+		System.out.println(str1+"는 숫자입니까? "+isNumber(str1));
+		str1 = "1234o";
+		System.out.println(str1+"는 숫자입니까? "+isNumber(str1));
+		
+		
 //		[실행결과]
 //		123는 숫자입니까? true
 //		1234o는 숫자입니까? false
@@ -342,11 +398,11 @@ public class Exam06 {
 		(1) max메서드를 작성하시오.
 		*/
 	
-//		int[] data = {3,2,9,4,7};
-//		System.out.println(java.util.Arrays.toString(data));
-//		System.out.println("최대값:"+max(data));
-//		System.out.println("최대값:"+max(null));
-//		System.out.println("최대값:"+max(new int[]{})); // 크기가 0인 배열
+		int[] data = {3,2,9,4,7};
+		System.out.println(java.util.Arrays.toString(data));
+		System.out.println("최대값:"+max(data));
+		System.out.println("최대값:"+max(null));
+		System.out.println("최대값:"+max(new int[]{})); // 크기가 0인 배열
 		
 //		[실행결과]
 //		[3, 2, 9, 4, 7]
@@ -369,16 +425,17 @@ public class Exam06 {
 		*/
 		
 		
-//		int value = 5;
-//		System.out.println(value+"의 절대값:"+abs(value));
-//		value = -10;
-//		System.out.println(value+"의 절대값:"+abs(value));
+		int value = 5;
+		System.out.println(value+"의 절대값:"+abs(value));
+		value = -10;
+		System.out.println(value+"의 절대값:"+abs(value));
 		
 	
-		
-		/*[실행결과]
+		/*
+		[실행결과]
 		5의 절대값:5
-		-10의 절대값:10*/
+		-10의 절대값:10
+		*/
 		
 		
 		
@@ -432,7 +489,7 @@ class Student{	//6-3,6-4, 6-5 클래스
 		return name+","+no+","+kor+","+eng+","+math+","+getTotal()+","+getAverage();
 	}
 }
-class distance{
+class distance{  //6-6 클래스
 	int x1;
 	int y1;
 	int x;
@@ -444,7 +501,7 @@ class distance{
 	return Math.sqrt((x-x1)*(x-x1) + (y-y1)*(y-y1));
 	}
 }
-class MyPoint{
+class MyPoint{ //6-7 클래스 
 	
 		int x;
 		int y;			
@@ -454,11 +511,10 @@ class MyPoint{
 			}
 	double getDistance1(int x1, int y1){
 		return 	Math.sqrt((x-x1)*(x-x1)+(y-y1)*(y-y1));
-		
-	}
-		
-		
+		}
 }
+		
+
 		
 		
 

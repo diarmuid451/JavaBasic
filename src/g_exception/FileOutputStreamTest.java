9039ddfcf342ba0847abafdcb64fae7b88a6309c
diarmuid. J <diarmuid451@gmail.com>
2019-12-17 17:20:43 +0900

    2019-12-17
    h_useful

---
+++ src/g_exception/FileOutputStreamTest.java
@@ -0,0 +1,22 @@
+package g_exception;
+
+import java.io.*;
+
+public class FileOutputStreamTest {
+
+	public static void main(String[] args)throws IOException {
+	
+		FileOutputStream output=new FileOutputStream("e:/test.txt",false);
+        //true로 두면 이어서 쓰고 , false로 쓰면 새로 씀
+		OutputStreamWriter writer=new OutputStreamWriter(output,"UTF-8");
+        BufferedWriter out=new BufferedWriter(writer);
+        
+    
+            out.write("안녕");
+            
+        
+        
+        out.close();
+	}
+
+}
---
+++ src/정대석/Homework.java
@@ -0,0 +1,10 @@
+package 정대석;
+
+public class Homework {
+
+	public static void main(String[] args) {
+		//자바의 정석 3판 1권 p469 ~ p471까지 과제
+
+	}
+
+}
